5d410602b68f4caa13d34266774135350527a037
Minwoo Kim <45740420+minwooKim401@users.noreply.github.com>
2021-03-02 14:28:31 +0900

    news update

--- docs/js/wowchemy-init.js
+++ docs/js/wowchemy-init.js
@@ -50,7 +50,7 @@
   var wcDarkLightEnabled = false;
   var wcIsSiteThemeDark = false;
 
-  // js/wowchemy-init.js
+  // <stdin>
   window.wc = {
     darkLightEnabled: wcDarkLightEnabled,
     isSiteThemeDark: wcIsSiteThemeDark
